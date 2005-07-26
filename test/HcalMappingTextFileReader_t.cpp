f3bfb1340465b5722a5f56cf06c43b69d6019537
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-07-26 15:11:32 +0000

    Tested, functional mapping

---
+++ test/HcalMappingTextFileReader_t.cpp
@@ -0,0 +1,25 @@
+#include "CondFormats/HcalMapping/interface/HcalMappingTextFileReader.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  char* name="test.map";
+  if (argc>1) name=argv[1];
+
+  cms::hcal::HcalMapping hrm=cms::hcal::HcalMappingTextFileReader::readFromFile(name,true);
+
+  std::vector<cms::HcalDetId>::const_iterator i=hrm.detid_begin();
+  for ( ; i!=hrm.detid_end(); i++ ) {
+    if (i->rawId()==0) continue;
+    cout << *i << " <-> " << hrm.lookup(*i) << endl;
+  }
+
+  std::vector<cms::HcalTrigTowerDetId>::const_iterator j=hrm.trigger_detid_begin();
+  for ( ; j!=hrm.trigger_detid_end(); j++ ) {
+    if (j->rawId()==0) continue;
+    cout << *j << " <-> " << hrm.lookupTrigger(*j) << endl;
+  }
+
+  return 0;
+}
