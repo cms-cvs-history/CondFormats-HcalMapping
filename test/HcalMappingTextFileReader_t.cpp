e554be1c09363f57adfcbe30aede07a45dd0db1c
Jeremy Mans <sha1-8c13ed8543bcbb752234eba083938d877e748795@cern.ch>
2005-10-04 14:32:40 +0000

    Removed namespaces Changed return type of reader to auto_ptr

--- test/HcalMappingTextFileReader_t.cpp
+++ test/HcalMappingTextFileReader_t.cpp
@@ -7,18 +7,18 @@ int main(int argc, char* argv[]) {
   const char* name="test.map";
   if (argc>1) name=argv[1];
 
-  cms::hcal::HcalMapping hrm=cms::hcal::HcalMappingTextFileReader::readFromFile(name,true);
+  std::auto_ptr<HcalMapping> hrm=HcalMappingTextFileReader::readFromFile(name,true);
 
-  std::vector<cms::HcalDetId>::const_iterator i=hrm.detid_begin();
-  for ( ; i!=hrm.detid_end(); i++ ) {
+  std::vector<HcalDetId>::const_iterator i=hrm->detid_begin();
+  for ( ; i!=hrm->detid_end(); i++ ) {
     if (i->rawId()==0) continue;
-    cout << *i << " <-> " << hrm.lookup(*i) << endl;
+    cout << *i << " <-> " << hrm->lookup(*i) << endl;
   }
 
-  std::vector<cms::HcalTrigTowerDetId>::const_iterator j=hrm.trigger_detid_begin();
-  for ( ; j!=hrm.trigger_detid_end(); j++ ) {
+  std::vector<HcalTrigTowerDetId>::const_iterator j=hrm->trigger_detid_begin();
+  for ( ; j!=hrm->trigger_detid_end(); j++ ) {
     if (j->rawId()==0) continue;
-    cout << *j << " <-> " << hrm.lookupTrigger(*j) << endl;
+    cout << *j << " <-> " << hrm->lookupTrigger(*j) << endl;
   }
 
   return 0;
